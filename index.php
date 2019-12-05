b4fe0c1ffe8cfc6b123f96f7a55db857518f926e
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-05 10:04:56 +0100

    correction

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
 $departments = array(
-  02 => 'Aisne',
-  59 => 'Nord',
-  60 => 'Oise',
-  62 => 'Pas-de-Calais',
-  80 => 'Somme');
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>PHP partie3 exo6</title>
-  </head>
-  <body>
-    <?php
-      foreach($departments as $cle => $element){
-        echo 'Le département' . ' ' . $element . ' ' . 'a le numéro' . ' ' . $cle . '<br />';
-      }
-    ?>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>PHP partie3 exo6</title>
+</head>
+<body>
+  <?php foreach($departments as $key => $value){ ?>
+    <p>Le département <?php echo $value; ?> a le numéro <?= $key; ?></p>
+  <?php }; ?>
+</body>
 </html>
